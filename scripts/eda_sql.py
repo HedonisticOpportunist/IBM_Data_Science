6548360f941856cdc75b3f7bdcaf8051fc2cc5de
HedonisticOpportunist <fujitahibaraki@gmail.com>
2022-04-20 23:34:37 +0100

    Update eda_sql.py

--- scripts/eda_sql.py
+++ scripts/eda_sql.py
@@ -13,25 +13,25 @@ connection = db.connect('Space.db')
 # data_frame.to_sql('SPACEXTBL', connection)
 
 # TASK 1
-all_launch_sites = pd.read_sql('SELECT * FROM SPACEXTBL', connection)
-all_launch_sites
-
 unique_launch_sites = pd.read_sql('SELECT DISTINCT Launch_Site FROM SPACEXTBL', connection)
-unique_launch_sites
 
 # TASK 2
 records_beginning_with_cca = pd.read_sql("SELECT * FROM SPACEXTBL WHERE Launch_Site LIKE 'CCA%' LIMIT 5", connection)
-records_beginning_with_cca
 
 # TASK 3
 total_mass = pd.read_sql("SELECT SUM(PAYLOAD_MASS__KG_) FROM SPACEXTBL", connection)
-total_mass
 
 # TASK 4
 avg_mass = pd.read_sql("SELECT AVG(PAYLOAD_MASS__KG_) FROM SPACEXTBL", connection)
-avg_mass
 
 # TASK 5
 first_successful_landing = pd.read_sql("SELECT MIN(DATE) FROM SPACEXTBL WHERE Mission_Outcome = 'Success'", connection)
-first_successful_landing
 
+# TASK 6
+boosters = pd.read_sql("SELECT DISTINCT Booster_Version FROM SPACEXTBL WHERE MISSION_OUTCOME = 'Success' AND PAYLOAD_MASS__KG_ BETWEEN 4000 AND 6000", connection)
+
+# TASK 7
+total_outcomes = pd.read_sql("SELECT COUNT(Mission_Outcome) FROM SPACEXTBL", connection)
+
+# TASK 8
+booster_versions_max = pd.read_sql("SELECT Booster_Version FROM SPACEXTBL WHERE PAYLOAD_MASS__KG_=(SELECT MAX(PAYLOAD_MASS__KG_) FROM SPACEXTBL)", connection)
