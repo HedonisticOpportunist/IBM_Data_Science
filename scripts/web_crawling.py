4bd0b58494b2a774c303da4e7ae03e158e115c70
HedonisticOpportunist <fujitahibaraki@gmail.com>
2022-04-21 00:30:02 +0100

    Update web_crawling.py

--- scripts/web_crawling.py
+++ scripts/web_crawling.py
@@ -8,3 +8,8 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 # Use soup.title attribute to verify that this works
 soup.title
+
+# TASK 2
+# Use the find_all function in the BeautifulSoup object, with element type `table`
+# Assign the result to a list called `html_tables`
+html_tables = soup.find_all('table')
