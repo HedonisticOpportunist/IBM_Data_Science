bbde47c658d96b7e39644adbb243eb325a6cbc40
HedonisticOpportunist <fujitahibaraki@gmail.com>
2022-04-21 00:29:07 +0100

    Update web_crawling.py

--- scripts/web_crawling.py
+++ scripts/web_crawling.py
@@ -1,1 +1,10 @@
+# TAsK 1
+# use requests.get() method with the provided static_url
+# assign the response to a object
+response = requests.get(static_url)
 
+# Use BeautifulSoup() to create a BeautifulSoup object from a response text content
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Use soup.title attribute to verify that this works
+soup.title
