fd31d0f4448d2220069f0fd5731b58f8ccdcfc4d
HedonisticOpportunist <fujitahibaraki@gmail.com>
2022-04-21 00:39:52 +0100

    Update web_crawling.py

--- scripts/web_crawling.py
+++ scripts/web_crawling.py
@@ -13,3 +13,13 @@ soup.title
 # Use the find_all function in the BeautifulSoup object, with element type `table`
 # Assign the result to a list called `html_tables`
 html_tables = soup.find_all('table')
+
+column_names = []
+
+# Apply find_all() function with `th` element on first_launch_table
+# Iterate each th element and apply the provided extract_column_from_header() to get a column name
+# Append the Non-empty column name (`if name is not None and len(name) > 0`) into a list called column_names
+for row in first_launch_table.find_all('th'):
+    name = extract_column_from_header(row)
+    if (name != None and len(name) > 0):
+        column_names.append(name)
