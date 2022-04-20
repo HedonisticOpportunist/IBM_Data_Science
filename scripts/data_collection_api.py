4dd5fd41d2d7e5539e189814af529b2c024b91fd
HedonisticOpportunist <fujitahibaraki@gmail.com>
2022-04-21 00:16:15 +0100

    Update data_collection_api.py

--- scripts/data_collection_api.py
+++ scripts/data_collection_api.py
@@ -1,5 +1,4 @@
 # TASK 1
-
 static_json_url='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/API_call_spacex_api.json'
 # print(response.status_code)
 
@@ -18,5 +17,8 @@ revised_df = pd.DataFrame.from_dict(launch_dict, orient='index', dtype=None).dro
 # Show the head of the dataframe
 revised_df.describe()
 
+# TASK 2
 # Filter to only display Falcon 9 launches 
 data_falcon9 = revised_df['BoosterVersion' != 'Falcon 1']
+
+# TASK 3
