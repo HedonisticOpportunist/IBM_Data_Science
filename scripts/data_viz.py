8d2ed864df4336a9903d8ffbc50ae0f84868609d
HedonisticOpportunist <fujitahibaraki@gmail.com>
2022-04-21 10:05:05 +0100

    Update data_viz.py

--- scripts/data_viz.py
+++ scripts/data_viz.py
@@ -4,3 +4,17 @@ sns.catplot(y='LaunchSite', x='FlightNumber', hue='Class', data=df, aspect=2)
 plt.xlabel("Flight Number", fontsize=20)
 plt.ylabel("Launch site", fontsize=20)
 plt.show()
+
+# TASK 2
+# Plot a scatter point chart with x axis to be Pay Load Mass (kg) and y axis to be the launch site, and hue to be the class value
+sns.scatterplot(x='PayloadMass', y='LaunchSite', hue='Class', data=df)
+plt.xlabel("Payload Mass kg", fontsize=20)
+plt.ylabel("Launch Site", fontsize=20)
+plt.show()
+
+# TASK 4
+# Plot a scatter point chart with x axis to be FlightNumber and y axis to be the Orbit, and hue to be the class value
+sns.catplot(x="FlightNumber", y="Orbit", hue='Class' , data=df, aspect=2)
+plt.xlabel("Flight Number", fontsize=15)
+plt.ylabel("Orbit", fontsize=15)
+plt.show()
